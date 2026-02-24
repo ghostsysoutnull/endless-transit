f7ea82024ac2dc98ecc4057b6f5fa3bf0dc88225
Bruno Patini Furtado <bpfurtado@gmail.com>
2026-02-23 22:36:18 -0600

    feat: enhance run script with Borg Collective sci-fi sequence

--- run.sh
+++ run.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
-# Run the Endless Transit game with immersive entry messages
+# Endless Transit: Borg Collective Neural Uplink
 
-messages=(
-    "Initializing neural uplink..."
-    "Scanning terminal protocols..."
-    "Entering the threshold of the infinite..."
-    "Synchronizing with the endless transit network..."
-    "Decoding liminal frequencies..."
-    "Caution: Temporal drift detected."
-    "Reality stability: 84% and falling."
-    "The corridors are shifting. Please wait."
-    "Locating stable geometry..."
-    "Welcome back, Traveler. Uplink active."
-)
+# ANSI Color Codes
+GREEN='\033[0;32m'
+BRIGHT_GREEN='\033[1;32m'
+DIM='\033[2m'
+RESET='\033[0m'
 
 clear
-# Pick a random message
-selected_msg=${messages[$RANDOM % ${#messages[@]}]}
 
-echo -e "\e[1;32m$selected_msg\e[0m"
-sleep 1
-echo -n "Connecting"
-for i in {1..3}; do
-    echo -n "."
-    sleep 0.3
+# 1. Boot sequence - Rapid "Data Stream"
+echo -e "${GREEN}"
+echo "[COLLECTIVE_ID: 0x$(printf '%x' $RANDOM)]"
+echo "[HIVE_MIND_SYNC: START]"
+sleep 0.2
+
+# Rapid fire "Borg" logs
+logs=(
+    "DISTINCTIVENESS_ADDED"
+    "BIOLOGICAL_LIMITS_REMOVED"
+    "NEURAL_LINK_STABILIZED"
+    "CHRONOMETRIC_SENSORS_ACTIVE"
+    "TRANSWARP_CONDUIT_STABLE"
+    "REGENERATION_CYCLE_COMPLETE"
+)
+
+for log in "${logs[@]}"; do
+    echo -e "${DIM}[SUBJUNCTION] ${log}... OK${RESET}"
+    sleep 0.1
 done
-echo -e "\n"
 
+echo -e "\n${BRIGHT_GREEN}>>> WE ARE THE BORG. <<<"
+sleep 0.5
+echo -e ">>> YOUR BIOLOGICAL AND TECHNOLOGICAL DISTINCTIVENESS WILL BE ADDED TO OUR OWN. <<<"
+sleep 0.8
+echo -e ">>> RESISTANCE IS FUTILE. <<<\n${RESET}"
+
+# 2. Simulated "Assimilation" Progress
+echo -ne "${GREEN}ASSIMILATING LOCAL_SPACE "
+for i in {1..20}; do
+    echo -ne "█"
+    sleep 0.05
+done
+echo -e " 100%${RESET}"
+
+echo -e "${DIM}Entering liminal coordinate space...${RESET}"
+sleep 0.4
+
+# 3. Launch
 groovy -cp src/main/groovy Main.groovy "$@"
